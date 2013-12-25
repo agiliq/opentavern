b4f46918ec7434874559478eb6e9f40c26b6e5bb
Shabda Raaj <shabda@agiliq.com>
2013-12-25 13:25:03 +0530

    Added some models we are going to need

--- tavern/models.py
+++ tavern/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 
 class Group(models.Model):
+    "Similar interests group, create events for these"
     name = models.CharField(max_length=40)
     group_type = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
@@ -9,5 +12,31 @@ class Group(models.Model):
     country = models.CharField(max_length=20)
     city = models.CharField(max_length=20)
 
+    organizers = models.ManyToManyField(User)
+
     def __unicode__(self):
         return self.name
+
+
+class Event(models.Model):
+    "Event you can attend"
+    group = models.ForeignKey(Group)
+
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    starts_at = models.DateTimeField()
+    end_at = models.DateTimeField(null=True, blank=True)
+    location = models.TextField(null=True, blank=True)
+
+    creator = models.ForeignKey(User)
+
+
+class Attendee(models.Model):
+    "People who have RSVPed to events"
+    user = models.ForeignKey(User)
+    user = models.ForeignKey(Event)
+    rsvped_on = models.DateTimeField()
+
+
+
+
