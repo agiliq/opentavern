5ebae5fa75ed54b57bfb57ce9a9bf74b68f38d61
krvc <krvc.it@gmail.com>
2013-12-26 19:45:06 +0530

    Added static url pattern

--- opentavern/urls.py
+++ opentavern/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 
 from tavern import views
 
@@ -9,3 +11,6 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', views.index, name='index'),
                        )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
