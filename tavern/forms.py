230ee5bd9c514f0cb2e2e6b5893f8dada503bd73
krvc <krvc.it@gmail.com>
2013-12-30 02:03:11 +0530

    Modified form

--- tavern/forms.py
+++ tavern/forms.py
@@ -1,10 +1,10 @@
 """ Opentavern Forms """
 
 from django.forms import ModelForm
-from .models import Group
+from .models import TavernGroup
 
 
 class CreateGroupForm(ModelForm):
     """ CreateGroupForm """
     class Meta:
-        model = Group
+        model = TavernGroup
