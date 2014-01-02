e8a10f3c0a0326d051d09004ed438b86b42fd07a
krvc <krvc.it@gmail.com>
2014-01-03 02:51:10 +0530

    Updated urls

--- opentavern/urls.py
+++ opentavern/urls.py
@@ -13,7 +13,10 @@ urlpatterns = patterns('',
                        url(r'^accounts/', include("django.contrib.auth.urls")),
                        url(r'^groups/(?P<group_id>\d+)', views.group_details, name='group_details'),
                        url(r'^events/(?P<event_id>\d+)', views.event_details, name='event_details'),
-                       url(r'^create_group/', views.create_group, name='create_group')
+                       url(r'^create_group/', views.create_group, name='create_group'),
+                       url(r'^(?P<pk>\d+)/tavern_group_update', views.tavern_group_update, name='tavern_group_update'),
+                       url(r'^create_event/', views.create_event, name='create_event'),
+                       url(r'^(?P<pk>\d+)/tavern_event_update', views.tavern_event_update, name='tavern_event_update'),
                        )
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
