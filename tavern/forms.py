f06dbd53790bda8a6180eaa86bf0b0c691b2cc78
krvc <krvc.it@gmail.com>
2014-01-03 02:50:40 +0530

    Added forms

--- tavern/forms.py
+++ tavern/forms.py
@@ -1,10 +1,19 @@
 """ Opentavern Forms """
 
 from django.forms import ModelForm
-from .models import TavernGroup
+from .models import TavernGroup, Event
 
 
 class CreateGroupForm(ModelForm):
     """ CreateGroupForm """
     class Meta:
         model = TavernGroup
+        exclude = ['creator', 'organizers']
+
+
+class CreateEventForm(ModelForm):
+    """ CreateEventForm """
+    class Meta:
+        model = Event
+        exclude = ['creator']
+
