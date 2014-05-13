29262cd4ea9e37ac3b4332bd48a47baba8d14cd4
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-13 16:57:05 +0530

    added settings file

--- opentavern/config/settings_base.py
+++ opentavern/config/settings_base.py
@@ -141,10 +141,3 @@ SOCIALACCOUNT_PROVIDERS = {
         'METHOD': 'oauth2',
     }
 }
-
-
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
