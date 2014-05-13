ed844652291da30e96db2aedd088305f10388068
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-13 15:51:39 +0530

    first commit to heroku

--- opentavern/wsgi.py
+++ opentavern/wsgi.py
@@ -1,14 +1,4 @@
-"""
-WSGI config for opentavern project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opentavern.settings")
-
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
