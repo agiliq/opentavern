cdb4ff9aced601e36259bfb500c667d3d5cdd66a
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-13 16:31:28 +0530

    revert back

--- opentavern/config/settings_base.py
+++ opentavern/config/settings_base.py
@@ -144,7 +144,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config(default=os.environ['DATABASE_URL']))
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
