c579e471bb31596be01aa5edfbb76c5307f54dd8
manjunath24 <hugar.manjunathrt@gmail.com>
2014-05-17 00:12:00 +0530

    minor hange

--- opentavern/wsgi.py
+++ opentavern/wsgi.py
@@ -4,7 +4,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opentavern.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-if "herokuapp" in os.environ:
-    from dj_static import Cling
 
-    application = Cling(get_wsgi_application())
