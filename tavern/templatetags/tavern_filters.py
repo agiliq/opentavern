cc4cadc47121e712607d1ed8740936ab2dd2fcc8
omkarvijay5 <omkarvijay5@gmail.com>
2014-09-12 17:25:37 +0530

    added a template tag get_all_tavern_groups which fetches all the tavern groups

--- tavern/templatetags/tavern_filters.py
+++ tavern/templatetags/tavern_filters.py
@@ -20,9 +20,25 @@ class UserTavernGroups(template.Node):
         return ''
 
 
+class AllTavernGroups(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+
+    def render(self, context):
+        all_groups = TavernGroup.objects.all()
+        context[self.var_name] = all_groups
+        return ''
+
 @register.tag
 def get_user_tavern_groups(parser, token):
     tag_name, arg = token.contents.split(None, 1)
     m = re.search(r'for (\w+.\w+) as (\w+)', arg)
     user, var_name = m.groups()
     return UserTavernGroups(user, var_name)
+
+@register.tag
+def get_all_tavern_groups(parser, token):
+    tag_name, arg = token.contents.split(None, 1)
+    m = re.search(r'as (\w+)', arg)
+    var_name = m.groups()[0]
+    return AllTavernGroups(var_name)
