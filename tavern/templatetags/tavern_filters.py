ce8ed3aab4a7f57d9e570cea6ead321bf4e347ef
omkarvijay5 <omkarvijay5@gmail.com>
2014-09-12 16:58:40 +0530

    used template.Variable to evaluate request.user

--- tavern/templatetags/tavern_filters.py
+++ tavern/templatetags/tavern_filters.py
@@ -5,16 +5,16 @@ from django import template
 from tavern.models import TavernGroup
 from tavern.models import get_groups
 
-
 register = template.Library()
 
 
 class UserTavernGroups(template.Node):
-    def __init__(self, var_name):
+    def __init__(self, user, var_name):
+        self.user = template.Variable(user)
         self.var_name = var_name
 
     def render(self, context):
-        user = context['user']
+        user = self.user.resolve(context)
         user_groups = get_groups(user)
         context[self.var_name] = user_groups
         return ''
@@ -25,4 +25,4 @@ def get_user_tavern_groups(parser, token):
     tag_name, arg = token.contents.split(None, 1)
     m = re.search(r'for (\w+.\w+) as (\w+)', arg)
     user, var_name = m.groups()
-    return UserTavernGroups(var_name)
+    return UserTavernGroups(user, var_name)
