a884a36a26b95ba66f6273df08c7124e99b27fae
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-09-13 03:44:11 +0530

    Fixing pep8 issues and removing print statements

--- tavern/templatetags/tavern_unjoined_filters.py
+++ tavern/templatetags/tavern_unjoined_filters.py
@@ -8,20 +8,19 @@ register = template.Library()
 
 
 class UserUnjoinedTavernGroup(template.Node):
-	def __init__(self, user, var_name):
-		self.user = template.Variable(user)
-		self.var_name = var_name
+    def __init__(self, user, var_name):
+        self.user = template.Variable(user)
+        self.var_name = var_name
 
-	def render(self,context):
-		user = self.user.resolve(context)
-		user_unjoined_groups = get_unjoined_groups(user)
-		context[self.var_name] = user_unjoined_groups
-		return ''
+    def render(self,context):
+        user = self.user.resolve(context)
+        user_unjoined_groups = get_unjoined_groups(user)
+        context[self.var_name] = user_unjoined_groups
+        return ''
 
 @register.tag
 def get_user_tavern_unjoined_groups(parser,token):
-	tag_name, arg = token.contents.split(None, 1)
-	m = re.search(r'for (\w+.\w+) as (\w+)', arg)
-	print m
-	user, var_name = m.groups()
-	return UserUnjoinedTavernGroup(user, var_name)
+    tag_name, arg = token.contents.split(None, 1)
+    m = re.search(r'for (\w+.\w+) as (\w+)', arg)
+    user, var_name = m.groups()
+    return UserUnjoinedTavernGroup(user, var_name)
