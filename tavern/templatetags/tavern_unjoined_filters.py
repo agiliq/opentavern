2b8ce50187f57c62dc803f425334299502038852
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-09-13 00:02:16 +0530

    Added template tag Node for unjoined groups for a user

--- tavern/templatetags/tavern_unjoined_filters.py
+++ tavern/templatetags/tavern_unjoined_filters.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 
 from tavern.models import get_unjoined_groups
@@ -5,7 +7,21 @@ from tavern.models import get_unjoined_groups
 register = template.Library()
 
 
-@register.assignment_tag
-def get_user_tavern_unjoined_groups(user):
-    user_unjoined_groups = get_unjoined_groups(user)
-    return user_unjoined_groups
+class UserUnjoinedTavernGroup(template.Node):
+	def __init__(self, user, var_name):
+		self.user = template.Variable(user)
+		self.var_name = var_name
+
+	def render(self,context):
+		user = self.user.resolve(context)
+		user_unjoined_groups = get_unjoined_groups(user)
+		context[self.var_name] = user_unjoined_groups
+		return ''
+
+@register.tag
+def get_user_tavern_unjoined_groups(parser,token):
+	tag_name, arg = token.contents.split(None, 1)
+	m = re.search(r'for (\w+.\w+) as (\w+)', arg)
+	print m
+	user, var_name = m.groups()
+	return UserUnjoinedTavernGroup(user, var_name)
