d7e860d440bc8024048364b0119b77f113985593
omkarvijay5 <omkarvijay5@gmail.com>
2014-09-12 16:48:02 +0530

    changed get_user_tavern_groups templatetag to template node

--- tavern/templatetags/tavern_filters.py
+++ tavern/templatetags/tavern_filters.py
@@ -1,11 +1,28 @@
+import re
+
 from django import template
 
+from tavern.models import TavernGroup
 from tavern.models import get_groups
+
 
 register = template.Library()
 
 
-@register.assignment_tag
-def get_user_tavern_groups(user):
-    user_groups = get_groups(user)
-    return user_groups
+class UserTavernGroups(template.Node):
+    def __init__(self, var_name):
+        self.var_name = var_name
+
+    def render(self, context):
+        user = context['user']
+        user_groups = get_groups(user)
+        context[self.var_name] = user_groups
+        return ''
+
+
+@register.tag
+def get_user_tavern_groups(parser, token):
+    tag_name, arg = token.contents.split(None, 1)
+    m = re.search(r'for (\w+.\w+) as (\w+)', arg)
+    user, var_name = m.groups()
+    return UserTavernGroups(var_name)
