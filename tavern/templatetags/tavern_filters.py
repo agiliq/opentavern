bc1ed7014040eb1d61b5f900cac3324de7f06261
omkarvijay5 <omkarvijay5@gmail.com>
2014-09-15 14:55:11 +0530

    added template tag get user tavern rsvp yes events and added mixins for template tags

--- tavern/templatetags/tavern_filters.py
+++ tavern/templatetags/tavern_filters.py
@@ -8,10 +8,15 @@ from tavern.models import get_groups
 register = template.Library()
 
 
-class UserTavernGroups(template.Node):
+class UserTemplateTagMixin(template.Node):
     def __init__(self, user, var_name):
         self.user = template.Variable(user)
         self.var_name = var_name
+
+
+class UserTavernGroups(UserTemplateTagMixin, template.Node):
+    def __init__(self, user, var_name):
+        super(UserTavernGroups, self).__init__(user, var_name)
 
     def render(self, context):
         user = self.user.resolve(context)
@@ -29,6 +34,20 @@ class AllTavernGroups(template.Node):
         context[self.var_name] = all_groups
         return ''
 
+
+class UserTavernRsvpEvents(UserTemplateTagMixin, template.Node):
+
+    def __init__(self, user, var_name):
+        super(UserTavernRsvpEvents, self).__init__(user, var_name)
+
+    def render(self, context):
+        user = self.user.resolve(context)
+        rsvp_yes_events = user.attendee_set.filter(rsvp_status='yes')
+        events = [event.event for event in rsvp_yes_events]
+        context[self.var_name] = events
+        return ''
+
+
 @register.tag
 def get_user_tavern_groups(parser, token):
     tag_name, arg = token.contents.split(None, 1)
@@ -36,9 +55,18 @@ def get_user_tavern_groups(parser, token):
     user, var_name = m.groups()
     return UserTavernGroups(user, var_name)
 
+
 @register.tag
 def get_all_tavern_groups(parser, token):
     tag_name, arg = token.contents.split(None, 1)
     m = re.search(r'as (\w+)', arg)
     var_name = m.groups()[0]
     return AllTavernGroups(var_name)
+
+
+@register.tag
+def get_user_tavern_rsvp_yes_events(parser, token):
+    tag_name, arg = token.contents.split(None, 1)
+    m = re.search(r'for (\w+.\w+) as (\w+)', arg)
+    user, var_name = m.groups()
+    return UserTavernRsvpEvents(user, var_name)
